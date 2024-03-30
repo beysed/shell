538895ff979525e4f07ad9f7d65cbcbdfa66a9d4
Serhii Kuzmychov <ku3cmich@gmail.com>
2024-03-30 10:21:38 +0200

    sync

--- execute/Execution_test.go
+++ execute/Execution_test.go
@@ -11,6 +11,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func execute(t *testing.T, exe Execution) (bool, string) {
+	hasOutput := false
+	output := ""
+
+	for run := true; run; {
+		select {
+		case o := <-exe.Stdout:
+			hasOutput = true
+			output = string(o)
+		case o := <-exe.Stderr:
+			fmt.Println(string(o))
+		case <-exe.Exit:
+			run = false
+		case <-time.After(time.Second * 3):
+			t.Error("exit not fired, process hags, timeout")
+			run = false
+		}
+	}
+
+	return hasOutput, output
+}
+
 func TestEnvironment(t *testing.T) {
 	var command Command
 
@@ -25,23 +47,10 @@ func TestEnvironment(t *testing.T) {
 	}
 
 	exe, _ := Execute(command, setup)
+	hasOutput, output := execute(t, exe)
 
-	hasOutput := false
-	for run := true; run; {
-		select {
-		case o := <-exe.Stdout:
-			assert.True(t, strings.HasPrefix(string(o), "MY"))
-			hasOutput = true
-		case o := <-exe.Stderr:
-			fmt.Println(string(o))
-		case <-exe.Exit:
-			run = false
-		case <-time.After(time.Second * 3):
-			t.Error("exit not fired, process hags, timeout")
-			run = false
-		}
-	}
 	assert.True(t, hasOutput)
+	assert.True(t, strings.HasPrefix(output, "MY"))
 }
 
 func TestConversationSed(t *testing.T) {
@@ -50,22 +59,9 @@ func TestConversationSed(t *testing.T) {
 	exe.Stdin <- []byte("sss")
 	close(exe.Stdin)
 
-	var output string
-	for run := true; run; {
-		select {
-		case o := <-exe.Stdout:
-			output += string(o)
-		case o := <-exe.Stderr:
-			fmt.Println(string(o))
-		case <-exe.Exit:
-			run = false
+	hasOutput, output := execute(t, exe)
 
-		case <-time.After(time.Second * 3):
-			t.Error("exit not fired, process hags, timeout")
-			run = false
-		}
-	}
-
+	assert.True(t, hasOutput)
 	assert.Equal(t, "Sss", string(output))
 }
 
@@ -75,21 +71,7 @@ func TestConversationCat(t *testing.T) {
 	exe.Stdin <- []byte{97, 97, 97}
 	close(exe.Stdin)
 
-	var output string
-	for run := true; run; {
-		select {
-		case o := <-exe.Stdout:
-			output += string(o)
-		case o := <-exe.Stderr:
-			fmt.Println(string(o))
-		case <-exe.Exit:
-			run = false
-
-		case <-time.After(time.Second * 3):
-			t.Error("exit not fired, process hags, timeout")
-			run = false
-		}
-	}
+	_, output := execute(t, exe)
 
 	assert.Equal(t, "aaa", string(output))
 }
@@ -107,18 +89,6 @@ func TestOutput(t *testing.T) {
 	}
 
 	exe, _ := Execute(MakeCommand(command, args...))
-
-	for run := true; run; {
-		select {
-		case o := <-exe.Stdout:
-			fmt.Print(string(o))
-
-		case <-exe.Exit:
-			run = false
-
-		case <-time.After(time.Second * 3):
-			t.Error("exit not fired, process hags, timeout")
-			run = false
-		}
-	}
+	hasOutput, _ := execute(t, exe)
+	assert.True(t, hasOutput)
 }
